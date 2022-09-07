979d0a43517f6fd392decb14639a1fe6e32016a3
Maxim Hladchuk <maxim.hladchuk@gmail.com>
2022-09-07 15:43:21 +0300

    second

--- app/code/CustomMax/main-page-popup/view/frontend/web/js/myscript.js
+++ app/code/CustomMax/main-page-popup/view/frontend/web/js/myscript.js
@@ -6,22 +6,34 @@ define (['jquery'],function($) {
     'use strict';
 
     return function () {
+        const bodyClass = 'cms-index-index';
+        const modalWindowContainerClass = '.main-page-modal-container';
+
+        /** check whether the user has already permanently closed the modal window through local storage data */
+
         if (!localStorage.getItem('modalVisibilityData')) {
             localStorage.setItem('modalVisibilityData', 'visible');
             toggleModalClass('hidden');
         }
 
+        /** check whether the user has already permanently closed the modal window through local storage data */
+        /** if the data from the local storage is visible, then we run the function of showing the modal window, and if it is hidden, then we hide the overflow of body */
+
         else if (localStorage.getItem('modalVisibilityData') == 'visible'){
             toggleModalClass('hidden');
         }else if (localStorage.getItem('modalVisibilityData') == 'invisible'){
-            $('.cms-index-index').css('overflow', "visible");
+            $(bodyClass).css('overflow', "visible");
         }
+
+        /** event to close modal window forever */
 
         $('#button-cancel-modal-fever').click(function () {
             event.preventDefault();
             localStorage.setItem('modalVisibilityData', 'invisible');
             toggleModalClass('visible');
         });
+
+        /** event to close modal window once */
 
         $('#button-cancel-modal-session').click(function () {
             event.preventDefault();
@@ -35,8 +47,8 @@ define (['jquery'],function($) {
          * @return void
          */
         function toggleModalClass(visibility) {
-            $('.main-page-modal-container').first().toggleClass('modal-display-block');
-            $('.cms-index-index').css('overflow', visibility);
+            $(modalWindowContainerClass).first().toggleClass('modal-display-block');
+            $(bodyClass).css('overflow', visibility);
         }
     }
 });
